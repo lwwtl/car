33f00d034661b9870dea50bb4e6898ff81729091
lwwtl <598449595@qq.com>
2021-01-27 18:09:52 +0800

    完成后台的订单管理

--- src/main/java/com/rlw/entity/Order.java
+++ src/main/java/com/rlw/entity/Order.java
@@ -1,11 +1,15 @@
 package com.rlw.entity;
 
 import java.math.BigDecimal;
+
+import com.alibaba.fastjson.annotation.JSONField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import java.time.LocalDateTime;
 import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
@@ -26,28 +30,41 @@ public class Order implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-
+    @JSONField(name="orderId")
     @TableId(value = "order_id", type = IdType.AUTO)
     private Long orderId;
 
+    @JSONField(name = "userId")
     private Long userId;
 
+    @JSONField(name = "carId")
     private Long carId;
 
+    @JSONField(name = "orderStart")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime orderStart;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JSONField(name = "orderEnd")
     private LocalDateTime orderEnd;
 
+    @JSONField(name = "orderSource")
     private String orderSource;
 
+    @JSONField(name = "orderDestination")
     private String orderDestination;
 
+    @JSONField(name = "orderRentDays")
     private Long orderRentDays;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JSONField(name = "orderCreate")
     private LocalDateTime orderCreate;
 
+    @JSONField(name = "orderState")
     private String orderState;
 
+    @JSONField(name = "orderTotal")
     private BigDecimal orderTotal;
 
 
